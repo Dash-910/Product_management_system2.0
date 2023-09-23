a70ebaecfa0783dddcc2361ed975ef0484292cdf
Akash <akashtale910@gmail.com>
2023-09-24 05:20:47 +0530

    Product and ProductDAO completed

--- src/java/com/manage/product/Product.java
+++ src/java/com/manage/product/Product.java
@@ -1,98 +1,93 @@
-
 package com.manage.product;
 
-
 public class Product {
-	    private String ProductName;
-	    private String ProductType;
-	    private String ProductPlace;
-	    private String ProductBrand;
-	    private double ProductPrice;
-	    private int ProductWarranty;
-	    
-	    
-		public Product(String productName, String productType, String productPlace, String productBrand,
-				double productPrice, int productWarranty) {
-			super();
-			ProductName = productName;
-			ProductType = productType;
-			ProductPlace = productPlace;
-			ProductBrand = productBrand;
-			ProductPrice = productPrice;
-			ProductWarranty = productWarranty;
-		}
 
+    private int ProductId;
+    private String ProductName;
+    private String ProductType;
+    private String ProductPlace;
+    private String ProductBrand;
+    private double ProductPrice;
+    private int ProductWarranty;
 
-		public Product() {
-			super();
-			// TODO Auto-generated constructor stub
-		}
+    public Product(int id, String productName, String productType, String productPlace, String productBrand,
+            double productPrice, int productWarranty) {
+        super();
+        ProductId = id;
+        ProductName = productName;
+        ProductType = productType;
+        ProductPlace = productPlace;
+        ProductBrand = productBrand;
+        ProductPrice = productPrice;
+        ProductWarranty = productWarranty;
+    }
 
+    public Product() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
-		public String getProductName() {
-			return ProductName;
-		}
+    public String getProductName() {
+        return ProductName;
+    }
 
+    public void setProductName(String productName) {
+        ProductName = productName;
+    }
 
-		public void setProductName(String productName) {
-			ProductName = productName;
-		}
+    public String getProductType() {
+        return ProductType;
+    }
 
+    public void setProductType(String productType) {
+        ProductType = productType;
+    }
 
-		public String getProductType() {
-			return ProductType;
-		}
+    public String getProductPlace() {
+        return ProductPlace;
+    }
 
+    public void setProductPlace(String productPlace) {
+        ProductPlace = productPlace;
+    }
 
-		public void setProductType(String productType) {
-			ProductType = productType;
-		}
+    public String getProductBrand() {
+        return ProductBrand;
+    }
 
+    public void setProductBrand(String productBrand) {
+        ProductBrand = productBrand;
+    }
 
-		public String getProductPlace() {
-			return ProductPlace;
-		}
+    public double getProductPrice() {
+        return ProductPrice;
+    }
 
+    public void setProductPrice(double productPrice) {
+        ProductPrice = productPrice;
+    }
 
-		public void setProductPlace(String productPlace) {
-			ProductPlace = productPlace;
-		}
+    public int getProductWarranty() {
+        return ProductWarranty;
+    }
 
+    public void setProductWarranty(int productWarranty) {
+        ProductWarranty = productWarranty;
+    }
 
-		public String getProductBrand() {
-			return ProductBrand;
-		}
+    public int getProductId() {
+        return ProductId;
+    }
 
+    public void setProductId(int productId) {
+        this.ProductId = productId;
+    }
 
-		public void setProductBrand(String productBrand) {
-			ProductBrand = productBrand;
-		}
-
-
-		public double getProductPrice() {
-			return ProductPrice;
-		}
-
-
-		public void setProductPrice(double productPrice) {
-			ProductPrice = productPrice;
-		}
-
-
-		public int getProductWarranty() {
-			return ProductWarranty;
-		}
-
-
-		public void setProductWarranty(int productWarranty) {
-			ProductWarranty = productWarranty;
-		}
-
-
-		@Override
+    @Override
 		public String toString() {
-			return "Product [ProductName=" + ProductName + ", ProductType=" + ProductType + ", ProductPlace="
+			return "Product [ProductId=" + ProductId +",ProductName=" + ProductName + ", ProductType=" + ProductType + ", ProductPlace="
 					+ ProductPlace + ", ProductBrand=" + ProductBrand + ", ProductPrice=" + ProductPrice
 					+ ", ProductWarranty=" + ProductWarranty + "]";
 		}    
+
 }
